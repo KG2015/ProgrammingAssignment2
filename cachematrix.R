a897636fe06f01fb9c64f09645e3677294f5f5ab
Kathryn Gilroy <kathryn.gilroy@gmail.com>
2015-01-22 12:01:33 +0000

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will cache the inverse of a matrix so that this
+## doesn't need to be repeatly computed. 
 
-## Write a short comment describing this function
+## The first function creates a matrix object and inverts it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                        m<-NULL
+                        set<-function(y) {
+                                x<<-y
+                                m<<-NULL
+                        } ## Sets value of the matrix
+                        
+                        get<-function() x ## Retrieves the value of the matrix
+                        setmatrix<-function(solve) m <<- solve ## Inverts the matrix
+                        getmatrix<-function() m ## Retrieves the value of the inverted matrix
+                        list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
+                        
 }
 
 
-## Write a short comment describing this function
+## The second function looks to see if the matrix has already been inverted.
+## If this has already been computed, this is retrieved from the cache. If not, the matrix is inverted and value returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+                m<-x$getmatrix()
+                if(!is.null(m)){
+                        message("getting cached data")
+                        return(m) ## 'If' function looks to see if matrix exists already; if does it returns 
+                                  ##it after telling you it's getting cached data
+                }
+                matrix<-x$get() ## Gets data from cached matrix
+                m<-solve(matrix,...) ## Calculates the inverse of the matrix if it wasn't already there
+                x$setmatrix(m) ## Sets value of inverse matrix in the cache
+                m ## returns the value of the inverse matrix
 }
